e0db77a374821846497d201294f7ad5a24be5b42
oobenm90 <mohamedbenhiba732@gmail.com>
2026-09-11 17:51:53 +0200

    Add files via upload

--- SSS.js
+++ SSS.js
@@ -51,6 +51,16 @@ serchView.classList.remove("hidden");
 });
 inicioLink.addEventListener("click", function(event){
 	event.preventDefault();
+	let offers = [{name: "john smith", image: "foto.jpg", oldPrice: "electricidad", newPrice: "terrassa", link: "https://wa.me/34635188642"},
+{name: "maria lopiz", image: "fotos.jpg", oldPrice: "psicologa", newPrice: "barcelona", link: "https://Www.netflix.com/inicio"},
+{name: "ahmed benhiba", image: "III.jpg", oldPrice: "abogado", newPrice: "badalona", link: "https://www.primevideo.com/offers/nonprimehomepage?_ssoLoop=1"},
+{name: "hbomax", image: "hbomax.jpg", oldPrice: "electrista", newPrice: "sabadell", link: "https//www.hbomax.com/es/es?gclsrc=aw.ds&gad_source=1&gad_campaignid=22339523404"},
+{name: "apple", image: "apple.jpg", oldPrice: "psicologa", newPrice: "rubí", link: "https://www.apple.com/es-es"},
+{name: "disney", image: "dsn.jpg", oldPrice: "abogado", newPrice: "gerona", link: " https://www.disney.com/es-es"}];
+let cardoffers = offers.map( function(offer){
+	return `<div class="colorOffer"><h2>${offer.name}</h2> <img class="FFF" src ="${offer.image}"> <p class="oldPrice">${offer.oldPrice}</p> <p class="newPrice">${offer.newPrice}</p> <a href ="${offer.link}">contactar</a></div>`;
+});
+document.getElementById("ofertCards").innerHTML = cardoffers.join("");
 	ofertCardsView.classList.remove("hidden");
 	perfilView.classList.add("hidden");
 	serchView.classList.add("hidden");
