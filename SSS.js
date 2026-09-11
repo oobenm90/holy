60afec7363eab73161ccbadd564073738eccec3e
oobenm90 <mohamedbenhiba732@gmail.com>
2026-09-11 23:59:25 +0200

    Add files via upload

--- SSS.js
+++ SSS.js
@@ -15,11 +15,17 @@ let offers = [{name: "john smith", image: "foto.jpg", oldPrice: "electricidad", 
 let cardoffers = offers.map( function(offer){
 	return `<div class="colorOffer"><h2>${offer.name}</h2> <img class="FFF" src ="${offer.image}"> <p class="oldPrice">${offer.oldPrice}</p> <p class="newPrice">${offer.newPrice}</p> <a href ="${offer.link}">contactar</a></div>`;
 });
+let filterResult = [{name: "john smith", image: "foto.jpg", oldPrice: "electricidad", newPrice: "terrassa", link: "https://wa.me/34635188642"},
+{name: "maria lopiz", image: "fotos.jpg", oldPrice: "psicologa", newPrice: "barcelona", link: "https://Www.netflix.com/inicio"},
+{name: "ahmed benhiba", image: "III.jpg", oldPrice: "abogado", newPrice: "badalona", link: "https://www.primevideo.com/offers/nonprimehomepage?_ssoLoop=1"},
+{name: "mohamed benhiba", image: "hbomax.jpg", oldPrice: "electrista", newPrice: "sabadell", link: "https//www.hbomax.com/es/es?gclsrc=aw.ds&gad_source=1&gad_campaignid=22339523404"},
+{name: "apple", image: "apple.jpg", oldPrice: "psicologa", newPrice: "rubí", link: "https://www.apple.com/es-es"},
+{name: "disney", image: "dsn.jpg", oldPrice: "abogado", newPrice: "gerona", link: " https://www.disney.com/es-es"}];
 document.getElementById("ofertCards").innerHTML = cardoffers.join("");
 let buttonSelect = document.getElementById("buttonSelect");
 buttonSelect.addEventListener("click", function(){
 let result = document.getElementById("servicioSelect").value;
-let resultados = offers.filter(name => name.oldPrice === result);
+let resultados = filterResult.filter(name => name.oldPrice === result);
 modofil.classList.add("hidden");
 let cardoffers = resultados.map( function(offer){
 	return `<div class="colorOffer"><h2>${offer.name}</h2> <img class="FFF" src ="${offer.image}"> <p class="oldPrice">${offer.oldPrice}</p> <p class="newPrice">${offer.newPrice}</p> <a href ="${offer.link}">contactar</a></div>`;
@@ -41,8 +47,9 @@ const modofil = document.getElementById("modofiltrar");
 serchLink.addEventListener("click", function(event){
 	event.preventDefault();
 let cardoffers = offers.map( function(offer){
-	return `<div class="colorOffer><h2>${offer.name}</h2> <img class="FFF" src ="${offer.image}"> <p class="oldPrice">${offer.oldPrice}</p> <p class="newPrice">${offer.newPrice}</p> <a href ="${offer.link}">contactar</a></div>`;
+	return `<div class="colorOffer"><h2>${offer.name}</h2> <img class="FFF" src ="${offer.image}"> <p class="oldPrice">${offer.oldPrice}</p> <p class="newPrice">${offer.newPrice}</p> <a href ="${offer.link}">contactar</a></div>`;
 });
+document.getElementById("ofertCards").innerHTML = cardoffers.join("");
 perfilView.classList.add("hidden");
 ofertCardsView.classList.remove("hidden");
 serchView.classList.remove("hidden");
@@ -57,6 +64,7 @@ inicioLink.addEventListener("click", function(event){
 let cardoffers = offers.map( function(offer){
 	return `<div class="colorOffer"><h2>${offer.name}</h2> <img class="FFF" src ="${offer.image}"> <p class="oldPrice">${offer.oldPrice}</p> <p class="newPrice">${offer.newPrice}</p> <a href ="${offer.link}">contactar</a></div>`;
 });
+document.getElementById("ofertCards").innerHTML = cardoffers.join("");
 document.getElementById("ofertCards").innerHTML = cardoffers.join("");
 	ofertCardsView.classList.remove("hidden");
 	perfilView.classList.add("hidden");
