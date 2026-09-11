6fcb2602378837d480cf7a0f114dbd117e1eae20
oobenm90 <mohamedbenhiba732@gmail.com>
2026-09-11 19:53:04 +0200

    Add files via upload

--- SSS.js
+++ SSS.js
@@ -9,7 +9,7 @@ planesCool = document.getElementById("planesCool").innerHTML = plan.join("");
 let offers = [{name: "john smith", image: "foto.jpg", oldPrice: "electricidad", newPrice: "terrassa", link: "https://wa.me/34635188642"},
 {name: "maria lopiz", image: "fotos.jpg", oldPrice: "psicologa", newPrice: "barcelona", link: "https://Www.netflix.com/inicio"},
 {name: "ahmed benhiba", image: "III.jpg", oldPrice: "abogado", newPrice: "badalona", link: "https://www.primevideo.com/offers/nonprimehomepage?_ssoLoop=1"},
-{name: "hbomax", image: "hbomax.jpg", oldPrice: "electrista", newPrice: "sabadell", link: "https//www.hbomax.com/es/es?gclsrc=aw.ds&gad_source=1&gad_campaignid=22339523404"},
+{name: "mohamed benhiba", image: "hbomax.jpg", oldPrice: "electrista", newPrice: "sabadell", link: "https//www.hbomax.com/es/es?gclsrc=aw.ds&gad_source=1&gad_campaignid=22339523404"},
 {name: "apple", image: "apple.jpg", oldPrice: "psicologa", newPrice: "rubí", link: "https://www.apple.com/es-es"},
 {name: "disney", image: "dsn.jpg", oldPrice: "abogado", newPrice: "gerona", link: " https://www.disney.com/es-es"}];
 let cardoffers = offers.map( function(offer){
@@ -41,7 +41,7 @@ const modofil = document.getElementById("modofiltrar");
 serchLink.addEventListener("click", function(event){
 	event.preventDefault();
 let cardoffers = offers.map( function(offer){
-	return `<div><h2>${offer.name}</h2> <img class="FFF" src ="${offer.image}"> <p class="oldPrice">${offer.oldPrice}</p> <p class="newPrice">${offer.newPrice}</p> <a href ="${offer.link}">contactar</a></div>`;
+	return `<div class="colorOffer><h2>${offer.name}</h2> <img class="FFF" src ="${offer.image}"> <p class="oldPrice">${offer.oldPrice}</p> <p class="newPrice">${offer.newPrice}</p> <a href ="${offer.link}">contactar</a></div>`;
 });
 perfilView.classList.add("hidden");
 ofertCardsView.classList.remove("hidden");
@@ -92,4 +92,8 @@ premiumBtn.addEventListener("click", function(event){
 });
 hola.addEventListener("click", function() {
 	modofil.classList.remove("hidden");
+});
+let buttonCerrar = document.getElementById("buttonCerrar");
+buttonCerrar.addEventListener("click", function(){
+	modofil.classList.add("hidden");
 });
