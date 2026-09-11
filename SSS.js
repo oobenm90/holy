69f47596964ea554a20bec16ac3ecfa190f76faf
oobenm90 <mohamedbenhiba732@gmail.com>
2026-09-11 17:44:03 +0200

    Add files via upload

--- SSS.js
+++ SSS.js
@@ -12,6 +12,10 @@ let offers = [{name: "john smith", image: "foto.jpg", oldPrice: "electricidad", 
 {name: "hbomax", image: "hbomax.jpg", oldPrice: "electrista", newPrice: "sabadell", link: "https//www.hbomax.com/es/es?gclsrc=aw.ds&gad_source=1&gad_campaignid=22339523404"},
 {name: "apple", image: "apple.jpg", oldPrice: "psicologa", newPrice: "rubí", link: "https://www.apple.com/es-es"},
 {name: "disney", image: "dsn.jpg", oldPrice: "abogado", newPrice: "gerona", link: " https://www.disney.com/es-es"}];
+let cardoffers = offers.map( function(offer){
+	return `<div class="colorOffer"><h2>${offer.name}</h2> <img class="FFF" src ="${offer.image}"> <p class="oldPrice">${offer.oldPrice}</p> <p class="newPrice">${offer.newPrice}</p> <a href ="${offer.link}">contactar</a></div>`;
+});
+document.getElementById("ofertCards").innerHTML = cardoffers.join("");
 let buttonSelect = document.getElementById("buttonSelect");
 buttonSelect.addEventListener("click", function(){
 let result = document.getElementById("servicioSelect").value;
